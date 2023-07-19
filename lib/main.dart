cd761a49511f9b486f1772dfaac8619e44f02a5b
HitoTei <s2313541@u.tsukuba.ac.jp>
2023-07-19 16:02:10 +0900

    Add shoplist

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,52 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('SHOKUHI（仮）'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: ShopList([
+        Shop(name: 'ロピア', tags: ['肉が安い', '冷食が安い']),
+        Shop(name: 'トライアル', tags: ['安そうで安い']),
+        Shop(name: 'カスミ', tags: ['行ったことない']),
+      ]),
+    );
+  }
+}
+
+class Shop {
+  const Shop({required this.name, required this.tags});
+
+  final String name;
+  final List<String> tags;
+}
+
+class ShopList extends StatelessWidget {
+  const ShopList(this.shopList, {super.key});
+  final List<Shop> shopList;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (var shop in shopList) ShopTile(shop),
+      ]
+    );
+  }
+}
+
+class ShopTile extends StatelessWidget {
+  const ShopTile(this.shop, {super.key});
+  final Shop shop;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(shop.name),
+      subtitle: Row(
+        children: [
+          for (var tag in shop.tags) Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue),
+            ),
+            child: Text(tag)
+          )
+        ],
       ),
     );
   }
