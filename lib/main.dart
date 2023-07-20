64bcf4462bbc642175bc5b6056356763995ba7f5
HitoTei <s2313541@u.tsukuba.ac.jp>
2023-07-20 11:51:07 +0900

    Change ShopList and shop model

--- lib/main.dart
+++ lib/main.dart
@@ -27,17 +27,60 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final shopList = [
+      Shop(
+        name: 'ロピア',
+        tags: ['肉が安い', '冷食が安い'],
+        address: '春日らへん',
+        telephoneNumber: '0120-107-929',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 1,
+          fish: 2,
+          vegetable: 3,
+          frozenFood: 4,
+          dairy: 5,
+        ),
+      ),
+      Shop(
+        name: 'トライアル',
+        tags: ['野菜が安い', '肉が安い'],
+        address: '学園の森',
+        telephoneNumber: '0120-',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 5,
+          fish: 4,
+          vegetable: 3,
+          frozenFood: 2,
+          dairy: 1,
+        ),
+      ),
+      Shop(
+        name: 'カスミ',
+        tags: ['卵が安い', '冷食が安い'],
+        address: '平砂',
+        telephoneNumber: '',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 2,
+          fish: 2,
+          vegetable: 2,
+          frozenFood: 2,
+          dairy: 2,
+        ),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('SHOKUHI（仮）'),
       ),
-      body: ShopList(
-        [
-          Shop(name: 'ロピア', tags: ['肉が安い', '冷食が安い'], evaluation: Evaluation(3, 1, 2)),
-          Shop(name: 'トライアル', tags: ['安そうで安い'], evaluation: Evaluation(3, 1, 2)),
-          Shop(name: 'カスミ', tags: ['行ったことない'], evaluation: Evaluation(3, 1, 2)),
-        ],
-      ),
+      body: ShopList(shopList),
     );
   }
 }
--- lib/ui/shop_list.dart
+++ lib/ui/shop_list.dart
@@ -12,7 +12,10 @@ class ShopList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        for (var shop in shopList) ShopTile(shop),
+        for (final shop in shopList)
+          ShopTile(
+            shop,
+          ),
       ],
     );
   }
@@ -25,44 +28,106 @@ class ShopTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(shop.name),
-      subtitle: Column(
+    final openTime = shop.openTime[0];
+    final closeTime = shop.closeTime[0];
+    return Card(
+      child: Column(
         children: [
           Row(
+            // タイトル + 営業時間
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              for (var tag in shop.tags)
-                Container(
-                  margin: const EdgeInsets.only(right: 4),
-                  child: Text(
-                    tag,
-                    style: const TextStyle(
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                )
+              Text(shop.name, style: const TextStyle(fontSize: 23)),
+              Text(
+                '${openTime.hour.toString().padLeft(2, '0')}:${openTime.minute.toString().padLeft(2, '0')} ~ ${closeTime.hour.toString().padLeft(2, '0')}:${closeTime.minute.toString().padLeft(2, '0')}',
+              ),
             ],
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text(' 肉'),
-              Text(shop.evaluation.meat.toString()),
-              const Text(' 魚'),
-              Text(shop.evaluation.fish.toString()),
-              const Text(' 野菜'),
-              Text(shop.evaluation.vegetable.toString()),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 評価
+                  Row(children: [
+                    const SizedBox(
+                      width: 100,
+                      child: Text('肉'),
+                    ),
+                    EvaluationWidget(shop.evaluation.meat)
+                  ]),
+                  Row(children: [
+                    const SizedBox(
+                      width: 100,
+                      child: Text('魚'),
+                    ),
+                    EvaluationWidget(shop.evaluation.fish)
+                  ]),
+                  Row(children: [
+                    const SizedBox(
+                      width: 100,
+                      child: Text('野菜'),
+                    ),
+                    EvaluationWidget(shop.evaluation.vegetable)
+                  ]),
+                  Row(children: [
+                    const SizedBox(
+                      width: 100,
+                      child: Text('冷食'),
+                    ),
+                    EvaluationWidget(shop.evaluation.frozenFood)
+                  ]),
+                  Row(children: [
+                    const SizedBox(
+                      width: 100,
+                      child: Text('卵・乳製品'),
+                    ),
+                    EvaluationWidget(shop.evaluation.dairy)
+                  ]),
+                ],
+              ),
+              Column(
+                children: [
+                  // その他の情報
+
+                  Row(
+                    children: [
+                      const Icon(Icons.home),
+                      Text(shop.address),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.phone),
+                      Text(shop.telephoneNumber),
+                    ],
+                  ),
+                  for (final tag in shop.tags)
+                    TextButton(onPressed: () {}, child: Text(tag)),
+                ],
+              ),
             ],
-          ),
+          )
         ],
       ),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ShopDetail(shop),
-          ),
-        );
-      },
+    );
+  }
+}
+
+class EvaluationWidget extends StatelessWidget {
+  const EvaluationWidget(this.evaluation, {super.key});
+
+  final int evaluation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        for (var i = 0; i < evaluation; i++) const Icon(Icons.star),
+        for (var i = evaluation; i < 5; i++) const Icon(Icons.star_border),
+      ],
     );
   }
 }
