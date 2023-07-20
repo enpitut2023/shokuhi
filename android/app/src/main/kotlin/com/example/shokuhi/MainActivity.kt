5754678e49b75b94f6dc3cdab5ae78ac38c6f40b
Taiki-Kobasako <69570864+Taiki-Kobasako@users.noreply.github.com>
2023-07-20 13:26:28 +0900

    Revert "Feature/firebase test"

--- android/app/src/main/kotlin/com/example/shokuhi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shokuhi/MainActivity.kt
@@ -3,7 +3,4 @@ package com.example.shokuhi
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    fun main() {
-        println("Hello, World!")
-    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+import 'model/shop.dart';
+import 'ui/shop_list.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(
-    const MyApp(),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,79 +12,75 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyWidget(),
+    return MaterialApp(
+      title: 'SHOKUHI（仮）',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
     );
   }
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  final TextEditingController _controller = TextEditingController();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Container(
-                height: double.infinity,
-                alignment: Alignment.topCenter,
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection('shop_list')
-                      .orderBy('shop_tel')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return const Text('エラーが発生しました');
-                    }
-                    if (!snapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    final list = snapshot.requireData.docs
-                        .map<String>((DocumentSnapshot document) {
-                      final documentData =
-                      document.data()! as Map<String, dynamic>;
-                      return documentData['shop_name']! as String;
-                    }).toList();
-
-                    final reverseList = list.reversed.toList();
-
-                    return ListView.builder(
-                      itemCount: reverseList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Center(
-                          child: Text(
-                            reverseList[index],
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-            )
-          ],
+    final shopList = [
+      Shop(
+        name: 'ロピア',
+        tags: ['肉が安い', '冷食が安い'],
+        address: '春日らへん',
+        telephoneNumber: '0120-107-929',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 1,
+          fish: 2,
+          vegetable: 3,
+          frozenFood: 4,
+          dairy: 5,
         ),
       ),
+      Shop(
+        name: 'トライアル',
+        tags: ['野菜が安い', '肉が安い'],
+        address: '学園の森',
+        telephoneNumber: '0120-',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 5,
+          fish: 4,
+          vegetable: 3,
+          frozenFood: 2,
+          dairy: 1,
+        ),
+      ),
+      Shop(
+        name: 'カスミ',
+        tags: ['卵が安い', '冷食が安い'],
+        address: '平砂',
+        telephoneNumber: '',
+        openTime: [DateTime.utc(0,0,0,9,0)],
+        closeTime: [DateTime.utc(0,0,0,19,0)],
+        evaluation: Evaluation(
+          meat: 2,
+          fish: 2,
+          vegetable: 2,
+          frozenFood: 2,
+          dairy: 2,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SHOKUHI（仮）'),
+      ),
+      body: ShopList(shopList),
     );
   }
 }
