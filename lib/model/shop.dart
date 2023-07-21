a02cfc0464b00fdd7238646757853245b06422e6
taiyou-sun <sun.bigroad1153@gmail.com>
2023-07-21 10:38:44 +0900

    shop.dartにTagクラスを追加しました

--- lib/model/shop.dart
+++ lib/model/shop.dart
@@ -2,30 +2,21 @@ class Shop {
   const Shop(
       {required this.name,
       required this.tags,
-      required this.evaluation,
       required this.address,
       required this.telephoneNumber,
       required this.openTime,
       required this.closeTime});
 
   final String name;
-  final List<String> tags;
-  final Evaluation evaluation;
+  final List<Tag> tags;
   final String address;
   final String telephoneNumber;
   final List<String> openTime; // 営業開始時間 0:月, 6:日
   final List<String> closeTime; // 営業終了時間 0:月, 6:日
 
-  Shop.fromMap(Map<String, dynamic> data)
+  Shop.fromMap(Map<String, dynamic> data, List<Tag> ts)
       : name = data['shop_name'],
-        tags = ['tag1', 'tag2'],
-        evaluation = Evaluation(
-          meat: 0,
-          fish: 0,
-          vegetable: 0,
-          frozenFood: 0,
-          dairy: 0,
-        ),
+        tags = ts,
         address = data['shop_address'],
         telephoneNumber = data['shop_tel'],
         openTime = [
@@ -44,21 +35,17 @@ class Shop {
           data['shop_close']['Wednesday'],
           data['shop_close']['Thursday'],
           data['shop_close']['Friday'],
-          data['shop_close']['Saturday'],
+          data['shop_close']['Saturdasy'],
         ];
 }
 
-class Evaluation {
-  Evaluation(
-      {required this.meat,
-      required this.fish,
-      required this.vegetable,
-      required this.frozenFood,
-      required this.dairy});
+class Tag {
+  Tag(
+      {required this.id,
+      required this.name,
+      required this.value);
 
-  int meat = 0;
-  int fish = 0;
-  int vegetable = 0;
-  int frozenFood = 0;
-  int dairy = 0;
+  final String id;
+  final String name;
+  final int value;
 }
