9625df6b095549f09cdf21386926b2ca5453a5fb
Taiki-Kobasako <69570864+Taiki-Kobasako@users.noreply.github.com>
2023-07-21 16:56:16 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+
 import 'model/shop.dart';
 import 'ui/shop_list.dart';
 
