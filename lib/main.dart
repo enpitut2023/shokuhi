deb57b179e0708868eea8b86fad1ceafc304f74a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-21 16:35:04 +0900

    追加に追加

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(netlify_iosBundleId),
+        title: const Text(String.fromEnvironment("netlify_iosBundleId")),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('shop_list').snapshots(),
